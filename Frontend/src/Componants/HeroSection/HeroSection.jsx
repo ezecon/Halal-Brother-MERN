a48d17861bb453ff1f027364cffd03b1782aea2f
ezecon <nationhexus@gmail.com>
2024-07-04 10:47:07 +0600

    first commit

--- Frontend/src/Componants/HeroSection/HeroSection.jsx
+++ Frontend/src/Componants/HeroSection/HeroSection.jsx
@@ -1,14 +1,12 @@
-
-
 export default function HeroSection() {
   return (
     <div>
-        <section id="hero" className="p-20">
-        <div className="hero container">
+      <section id="hero" className="p-10 md:p-20">
+        <div className="hero container mx-auto">
           <div className="text-left">
-            <h1>Hello, <span></span></h1>
-            <h1>Welcome to  <span></span></h1>
-            <h1 className="mb-8">Halal Brothers <span></span></h1>
+            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Hello, <span></span></h1>
+            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Welcome to <span></span></h1>
+            <h1 className="mb-8 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Halal Brothers <span></span></h1>
             <a className="mb-10 text-white get-button">Get Started</a>
           </div>
         </div>
